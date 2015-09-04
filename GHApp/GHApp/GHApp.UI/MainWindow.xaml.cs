a617abc6efb7d3009fcbb8a1bd0136e46ca0f618
Bart Sokol <sokol.bartosz@gmail.com>
2015-09-04 20:37:00 +0200

    UI bootstrap

---
+++ GHApp/GHApp/GHApp.UI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GHApp.UI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = this;
+		}
+
+		private List<string> _favourites = new List<string>
+		{
+			"shanselman\\SuperRepo",
+			"bartsokol\\WuPeEf",
+			"nikodemrafalski\\eRiXy"
+		};
+
+		public List<string> Favourites
+		{
+			get { return _favourites; }
+			set
+			{
+				_favourites = value;
+			}
+		}
+	}
+}
