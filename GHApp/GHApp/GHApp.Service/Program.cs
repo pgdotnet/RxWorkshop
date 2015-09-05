c68d4c77bafb4fe9db46099157cbeceb061ce2a1
Bart Sokol <sokol.bartosz@gmail.com>
2015-09-05 08:11:30 +0200

    Communication channel changes

--- GHApp/GHApp/GHApp.Service/Program.cs
+++ GHApp/GHApp/GHApp.Service/Program.cs
@@ -1,4 +1,6 @@
-﻿using GHApp.Communication;
+﻿using System;
+using System.Configuration;
+using GHApp.Communication;
 using GHApp.Contracts.Dto;
 using GHApp.Contracts.Queries;
 using GHApp.Contracts.Responses;
@@ -9,14 +11,27 @@ namespace GHApp.Service
 	{
 		public static void Main()
 		{
+			var serverAddress = ConfigurationManager.AppSettings.Get("ServerAddress");
+			var serverPort = int.Parse(ConfigurationManager.AppSettings.Get("ServerPort"));
+
+			var clientAddress = ConfigurationManager.AppSettings.Get("ClientAddress");
+			var clientPort = int.Parse(ConfigurationManager.AppSettings.Get("ClientPort"));
+
 			var cs = new UdpClientServer();
-			var serverChannel = new UdpCommunicationChannel(cs, new ChannelConfig { Address = "127.0.0.1", Port = 12345 });
-			var clientChannel = new UdpCommunicationChannel(cs, new ChannelConfig { Address = "127.0.0.1", Port = 12346 });
+			var serverChannel = new UdpCommunicationChannel(cs, new ChannelConfig { Address = serverAddress, Port = serverPort });
+			var clientChannel = new UdpCommunicationChannel(cs, new ChannelConfig { Address = clientAddress, Port = clientPort });
+
 			var listener = new Listener<UserQuery, UserResponse>(clientChannel, serverChannel);
-			listener.Listen(x => new UserResponse(x.Id, new User { Bio = "100%" }));
+			listener.Listen(GetUser);
 
 			var demos = new Demos();
 			demos.WatchRepoDemo();
+		}
+
+		private static UserResponse GetUser(UserQuery query)
+		{
+			Console.WriteLine("Got UserQuery({0}), Id = {1}", query.Name, query.Id);
+			return new UserResponse(query.Id, new User { Bio = "100%" });
 		}
 	}
 }
